44776696a9d55a62ebfaa9cbcbc2e936eb1517b1
Glamdring <glamd@abv.bg>
2015-03-21 19:05:25 +0200

    using webapp resource

--- async/code/servlet/src/main/java/com/test/TestServlet.java
+++ async/code/servlet/src/main/java/com/test/TestServlet.java
@@ -1,15 +1,15 @@
 package com.test;
 
 import java.io.BufferedInputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
-import org.apache.commons.io.IOUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
 
 @WebServlet(urlPatterns="/read")
 public class TestServlet extends HttpServlet {
@@ -20,6 +20,6 @@ public class TestServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
             IOException {
         resp.setContentType("text/html");
-        IOUtils.copy(new BufferedInputStream(new FileInputStream("C:\\workspace-any\\async-benchmark\\nodejs\\largefile")), resp.getOutputStream());
+        IOUtils.copy(new BufferedInputStream(req.getServletContext().getResourceAsStream("/largefile")), resp.getOutputStream());
     }
 }
